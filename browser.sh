144c28b06b4cbc821ad073d0c9f05bfba7c6e282
Harm van den Dorpel <send@harm.email>
2022-01-10 09:51:49 +0100

    ok

--- browser.sh
+++ browser.sh
@@ -1,3 +1,23 @@
-sleep 10
-chromium-browser --start-fullscreen --kiosk --enable-gpu-rasterization --enable-hardware-overlays http://localhost:3000
 
+#!/bin/sh
+sleep 10
+
+set -e
+
+CHROMIUM_TEMP=~/tmp/chromium
+rm -Rf ~/.config/chromium/
+rm -Rf $CHROMIUM_TEMP
+mkdir -p $CHROMIUM_TEMP
+
+chromium-browser \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-save-password-bubble \
+    --disk-cache-dir=$CHROMIUM_TEMP/cache/ \
+    --user-data-dir=$CHROMIUM_TEMP/user_data/ \
+    --start-maximized \
+    --kiosk "http://localhost:3000" &
+sleep 5
+# xdotool search --sync --onlyvisible --class "chromium" key F11
